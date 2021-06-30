7067c3d160c3289450b108407fcb703e51a9fb76
Girish Kumar <g.girish1026@gmail.com>
2021-06-30 06:03:15 +0000

    change in index.js file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 //Respond with "Hey world demo" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('Hey world!');
+ res.send('Hey world demo');
 });
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
