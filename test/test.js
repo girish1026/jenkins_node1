b7b3ecdea4af75b302d2f5cb4e7bf9ab63d0960e
Girish Kumar <g.girish1026@gmail.com>
2021-06-30 04:06:21 +0000

    comment added

--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@ var app = require('../index.js');
 describe('GET /', function() {
  it('respond with Hey world!', function(done) {
  //navigate to root and check the the response is "Hey world!"
+ //Small demo
  request(app).get('/').expect('Hey world!', done);
  });
 });
